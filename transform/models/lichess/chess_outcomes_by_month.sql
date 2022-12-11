15b8674ce65683d658130308d89cb471e4a833a6
Charles Julian Knight <charles@rabidaudio.com>
2022-12-11 16:38:56 -0500

    more lichess transforms

--- transform/models/lichess/chess_outcomes_by_month.sql
+++ transform/models/lichess/chess_outcomes_by_month.sql
@@ -2,20 +2,26 @@ with games as (
     select * from {{ ref('chess_games') }}
 ),
 
-openings_with_stats as (
+game_outcomes as (
     select
         username,
-        opening_name,
-        side,
+        date_trunc('month', created_at) as month,
+        outcome
+    from games
+),
+
+stats as (
+    select
+        username,
+        month,
         count(1) as total_games,
         sum(case when outcome = 'won' then 1 else 0 end) as total_won_games,
         sum(case when outcome = 'lost' then 1 else 0 end) as total_lost_games,
         sum(case when outcome = 'drawn' then 1 else 0 end) as total_drawn_games
-    from games
-    where opening_name is not null
-    group by username, opening_name, side
+    from game_outcomes
+    group by username, month
 )
 
 select *,
     cast((total_won_games - total_lost_games) as double) / cast(total_games as double) as score
-    from openings_with_stats
+from stats
